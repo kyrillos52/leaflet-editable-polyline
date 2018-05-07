e4488d0839b0d5e53c40cc8ae60f5b4e85c225b8
DESKTOP-JCULFC1\Supriya <supriya.sudhindra@politrontech.com>
2018-05-07 12:05:06 +0530

    deleted package.json

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/leaflet-editable-polyline.js',
-  output: {
-    filename: 'leaflet-editable-polyline.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-};
